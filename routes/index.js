6f483a3370ba74173eeb7e3c918465dd43ed300d
EC2 Default User <ec2-user@ip-172-31-5-63.ec2.internal>
2016-08-27 08:32:25 +0000

    random twitch get request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+request('https://api.twitch.tv/kraken/channels/test_channel', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(JSON.parse(body).views) // Show the HTML for the Google homepage. 
+  }
+})
 module.exports = router;
